9eb19eaa96eee03d308b3b39bcb4f665c56ef373
mickey <mickey>
2009-03-26 11:59:32 +0000

    one rcsid is enough (undef LIBC_SCCS)

--- lib/libc/gen/_sys_siglist.c
+++ lib/libc/gen/_sys_siglist.c
@@ -4,7 +4,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$ABSD$";
+static const char rcsid[] = "$ABSD: _sys_siglist.c,v 1.1.1.1 2008/08/26 14:38:27 root Exp $";
 #endif
 
 #include <sys/cdefs.h>
@@ -16,10 +16,12 @@ __indr_reference(_sys_siglist, __sys_siglist); /* Backwards compat with v.12 */
 
 #undef _sys_siglist
 #define	_sys_siglist	sys_siglist
+#undef LIBC_SCCS
 #include "siglist.c"
 
 #undef _sys_siglist
 #define	_sys_siglist	__sys_siglist
+#undef LIBC_SCCS
 #include "siglist.c"
 
 #endif
