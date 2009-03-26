42a4d26dc1fd3527a8cc47e78022de7bd65aa3e2
mickey <mickey>
2009-03-26 11:50:43 +0000

    one rcsid is enough (undef LIBC_SCCS)

--- lib/libc/gen/_sys_errlist.c
+++ lib/libc/gen/_sys_errlist.c
@@ -4,7 +4,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$ABSD$";
+static const char rcsid[] = "$ABSD: _sys_errlist.c,v 1.1.1.1 2008/08/26 14:38:27 root Exp $";
 #endif
 
 #include <sys/cdefs.h>
@@ -18,12 +18,14 @@ __indr_reference(_sys_errlist, __sys_errlist); /* Backwards compat with v.12 */
 #undef _sys_nerr
 #define	_sys_errlist	sys_errlist
 #define	_sys_nerr	sys_nerr
+#undef LIBC_SCCS
 #include "errlist.c"
 
 #undef _sys_errlist
 #undef _sys_nerr
 #define	_sys_errlist	__sys_errlist
 #define	_sys_nerr	__sys_nerr
+#undef LIBC_SCCS
 #include "errlist.c"
 
 #endif
