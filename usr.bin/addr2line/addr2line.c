e81479dacd71c8f4834ce961268c38edac308b53
mickey <mickey>
2009-04-15 12:43:22 +0000

    convert address into source file:line; work in progress

---
+++ usr.bin/addr2line/addr2line.c
@@ -0,0 +1,340 @@
+/*
+ * Copyright (c) 2009 Michael Shalayeff
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+static const char rcsid[] =
+    "$ABSD$";
+
+#include <sys/param.h>
+#include <a.out.h>
+#include <elf_abi.h>
+#include <stab.h>
+#include <ar.h>
+#include <ranlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+#include <ctype.h>
+#include <link.h>
+#ifdef __ELF__
+#include <link_aout.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <elfuncs.h>
+
+#include "byte.c"
+
+#define	A2LFUNAME	1
+#define	A2LBASENAME	2
+
+void usage(void);
+int addr2line(const char *, FILE *, long, char **, char **, int *);
+void a2lprintf(const char *, const char *, int, int);
+
+/* a funky nlist overload for reading 32bit a.out on 64bit toys */
+/* stolen from nm.c */
+struct nlist32 {
+	u_int32_t	strx;
+	u_int8_t	type;
+	u_int8_t	other;
+	u_int16_t	desc;
+	u_int32_t	value;
+} __packed;
+
+char *stab;	/* -lelf */
+
+int
+main(int argc, char *argv[])
+{
+	extern char *__progname;
+	extern int optind;
+	const char *fn;
+	FILE *fp;
+	char *ep, *fname, *funame;
+	long ptr;
+	int ch, flags, ln;
+
+	fn = "a.out";
+	flags = 0;
+	while ((ch = getopt(argc, argv, "e:fs")) != -1)
+		switch (ch) {
+		case 'e':
+			fn = optarg;
+			break;
+
+		case 'f':
+			flags |= A2LFUNAME;
+			break;
+
+		case 's':
+			flags |= A2LBASENAME;
+			break;
+
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (!(fp = fopen(fn, "r")))
+		err(1, "fopen: %s", fn);
+
+	if (*argv)
+		for (; *argv; argv++) {
+			errno = 0;
+			ptr = strtoul(*argv, &ep, 0);
+			if (*argv[0] == '\0' || *ep != '\0')
+				errx(1, "\'%s\' is ain't no number", *argv);
+			if (errno == ERANGE && ptr == ULONG_MAX)
+				errx(1, "\'%s\' address's out of there", *argv);
+
+			if (!addr2line(fn, fp, ptr, &fname, &funame, &ln))
+				a2lprintf(fname, funame, ln, flags);
+		}
+	else
+		while (scanf("%p", &ptr) != EOF)
+			if (!addr2line(fn, fp, ptr, &fname, &funame, &ln))
+				a2lprintf(fname, funame, ln, flags);
+
+	return 0;
+}
+
+void
+a2lprintf(const char *fname, const char *funame, int ln, int flags)
+{
+	char *p;
+
+	if ((flags & A2LBASENAME) &&
+	    (p = basename(fname)))
+		fname = p;
+
+	if (flags & A2LFUNAME)
+		printf("%s:%s:%d", fname, funame, ln);
+	else
+		printf("%s:%d", fname, ln);
+}
+
+void
+usage(void)
+{
+	extern char *__progname;
+
+	fprintf(stderr, "usage: %s [-fs] [-e a.out] [addr ...]\n", __progname);
+	exit(1);
+}
+
+int
+addr2line(const char *name, FILE *fp,long addr,char **pfn,char **pfun,int *pln)
+{
+	union hdr {
+		struct exec aout;
+		Elf32_Ehdr elf32;
+		Elf64_Ehdr elf64;
+	} head;
+	size_t bytes;
+	int rv;
+
+	bytes = fread((char *)&head, 1, sizeof(head), fp);
+	if (bytes < sizeof(head))
+		if (bytes < sizeof(head.aout) || IS_ELF(head.elf32)) {
+			warnx("%s: bad format", name);
+			fclose(fp);
+			return 1;
+		}
+
+	if (IS_ELF(head.elf32) &&
+	    head.elf32.e_ident[EI_CLASS] == ELFCLASS32 &&
+	    head.elf32.e_ident[EI_VERSION] == ELF_TARG_VER) {
+		Elf32_Shdr *shdr;
+		char *shstr;
+		int i;
+
+		elf32_fix_header(&head.elf32);
+		if (!(shdr = elf32_load_shdrs(name, fp, 0, &head.elf32)))
+			return 1;
+
+		elf32_fix_shdrs(&head.elf32, shdr);
+		if (!(shstr = elf32_shstrload(name, fp, 0, &head.elf32, shdr)))
+			return 1;
+
+		for (i = 0; i < head.elf32.e_shnum; i++)
+			if (!strcmp(shstr + shdr[i].sh_name, ".debug_line"))
+				break;
+
+		if (i < head.elf32.e_shnum) {
+			if (fseeko(fp, shdr[i].sh_offset, SEEK_SET)) {
+				warn("%s: fseeko", name);
+				free(shstr);
+				free(shdr);
+				return 1;
+			}
+
+			rv = elf32_addr2line(name, fp, addr, pfn, pfun, pln);
+		}
+
+		free(shstr);
+		free(shdr);
+
+	} else if (IS_ELF(head.elf64) &&
+	    head.elf64.e_ident[EI_CLASS] == ELFCLASS64 &&
+	    head.elf64.e_ident[EI_VERSION] == ELF_TARG_VER) {
+		Elf64_Shdr *shdr;
+		char *shstr;
+		int i;
+
+		elf64_fix_header(&head.elf64);
+		if (!(shdr = elf64_load_shdrs(name, fp, 0, &head.elf64)))
+			return 1;
+
+		elf64_fix_shdrs(&head.elf64, shdr);
+		if (!(shstr = elf64_shstrload(name, fp, 0, &head.elf64, shdr)))
+			return 1;
+
+		for (i = 0; i < head.elf64.e_shnum; i++)
+			if (!strcmp(shstr + shdr[i].sh_name, ".debug_line"))
+				break;
+
+		if (i < head.elf64.e_shnum) {
+			if (fseeko(fp, shdr[i].sh_offset, SEEK_SET)) {
+				warn("%s: fseeko", name);
+				free(shstr);
+				free(shdr);
+				return 1;
+			}
+
+			rv = elf64_addr2line(name, fp, addr, pfn, pfun, pln);
+		}
+
+		free(shstr);
+		free(shdr);
+
+	} else if (BAD_OBJECT(head.aout)) {
+		warnx("%s: bad format", name);
+		return 1;
+	} else {
+		struct nlist nl;
+		char *strtab;
+		u_int32_t w;
+		int nsym;
+
+		fix_header_order(&head.aout);
+		/* stop if the object file contains no symbol table */
+		if (!head.aout.a_syms) {
+			warnx("%s: no name list", name);
+			return 1;
+		}
+
+		if (fseeko(fp, head.aout.a_syms + N_SYMOFF(head.aout),
+		    SEEK_SET)) {
+			warn("%s: fseeko", name);
+			return 1;
+		}
+
+		/* get the string table size */
+		if (fread(&w, sizeof(w), (size_t)1, fp) != 1) {
+			warnx("%s: cannot read stab size", name);
+			return 1;
+		}
+
+		w = fix_32_order(w, N_GETMID(head.aout));
+		if (!(strtab = malloc(w))) {
+			warn("malloc: strtab (%u bytes)", w);
+			return 1;
+		}
+
+		if (fread(strtab, w, 1, fp) != 1) {
+			warn("%s: read stab", name);
+			return 1;
+		}
+
+		/* seek back to the symtab */
+		if (fseeko(fp, N_SYMOFF(head.aout), SEEK_SET)) {
+			warn("%s: fseeko", name);
+			return 1;
+		}
+
+		rv = 1;
+#ifdef __LP64__
+		for (nsym = head.aout.a_syms / sizeof(struct nlist32);
+		    nsym--; ) {
+			struct nlist32 nl32;
+
+			if (fread(&nl32, sizeof(nl32), 1, fp) != 1) {
+				warnx("%s: cannot read symbol table", name);
+				free(strtab);
+				return 1;
+			}
+
+			nl.n_type = nl32.type;
+			nl.n_other = nl32.other;
+			if (byte_sex(N_GETMID(head.aout)) != BYTE_ORDER) {
+				nl.n_un.n_strx = swap32(nl32.strx);
+				nl.n_desc = swap16(nl32.desc);
+				nl.n_value = swap32(nl32.value);
+			} else {
+				nl.n_un.n_strx = nl32.strx;
+				nl.n_desc = nl32.desc;
+				nl.n_value = nl32.value;
+			}
+#else
+		for (nsym = head.aout.a_syms / sizeof(nl); nsym--; ) {
+
+			if (fread(&nl, sizeof(nl), 1, fp) != 1) {
+				warnx("%s: cannot read symbol table", name);
+				free(strtab);
+				return 1;
+			}
+			if (byte_sex(N_GETMID(head.aout)) != BYTE_ORDER)
+				swap_nlist(&nl);
+#endif
+			nl.n_un.n_name = strtab + nl.n_un.n_strx;
+
+			switch (nl.n_type) {
+			case N_SO:
+			case N_SOL:
+				if (nl.n_value <= addr)
+					*pfn = nl.n_un.n_name;
+				break;
+
+			case N_TEXT:
+				if (nl.n_un.n_name[0] != '_')
+					break;
+				/* FALLTHROUGH */
+			case N_FUN:
+			case N_ENTRY:
+				if (nl.n_value <= addr)
+					*pfun = nl.n_un.n_name;
+				break;
+
+			case N_SLINE:
+				if (nl.n_value <= addr)
+					*pln = nl.n_desc;
+				break;
+			}
+		}
+
+		if (*pln > 0 && *pfn)
+			rv = 0;
+
+		free(strtab);
+	}
+
+	return rv;
+}
