87bbe2de775a94e85be6990467175f2f58ccdeba
mickey <mickey>
2009-05-27 22:13:06 +0000

    make static of a global inline function that is defined in an include file

--- usr.bin/make/targ.h
+++ usr.bin/make/targ.h
@@ -57,7 +57,7 @@ extern GNode *Targ_FindNodeih(const char *, const char *, uint32_t, int);
 
 extern inline GNode *
 Targ_FindNodeh(const char *, size_t, uint32_t, int);
-extern inline GNode *
+static inline GNode *
 Targ_FindNodeh(const char *name, size_t n, uint32_t hv, int flags)
 {
 	return Targ_FindNodeih(name, name + n - 1, hv, flags);
