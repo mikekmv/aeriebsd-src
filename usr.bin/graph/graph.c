180425cbddbab77a3652d7b74d8d82f3c1e82319
mickey <mickey>
2009-05-28 13:59:04 +0000

    remove 3rd clause form bsd license

--- usr.bin/graph/graph.c
+++ usr.bin/graph/graph.c
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgment:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -75,7 +71,7 @@ static const char copyright[] =
 static const char sccsid[] = "@(#)graph.c	8.1 (Berkeley) 6/6/93";
 #else
 static const char rcsid[] =
-    "$ABSD$";
+    "$ABSD: graph.c,v 1.1 2009/03/21 15:03:33 mickey Exp $";
 #endif
 #endif
 
