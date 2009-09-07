987f4772501be003cc661766edaa0832b0c4b37d
mickey <mickey>
2009-09-07 21:04:18 +0000

    fix spaces vs tabs and defines

--- sys/arch/i386/include/reloc.h
+++ sys/arch/i386/include/reloc.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2002,2003 Dale Rahn
  *
@@ -23,27 +22,26 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#ifndef  _MACH_RELOC_H 
-#define  _MACH_RELOC_H 
-#define R_TYPE(X)       __CONCAT(RELOC_,X)
 
+#ifndef	_I386_RELOC_H_
+#define	_I386_RELOC_H_
 
-#define RELOC_NONE      0   /* No reloc */
-#define RELOC_32        1   /* Direct 32 bit  */
-#define RELOC_PC32      2   /* PC relative 32 bit */
-#define RELOC_GOT32     3   /* 32 bit GOT entry */
-#define RELOC_PLT32     4   /* 32 bit PLT address */
-#define RELOC_COPY      5   /* Copy symbol at runtime */
-#define RELOC_GLOB_DAT  6   /* Create GOT entry */
-#define RELOC_JUMP_SLOT 7   /* Create PLT entry */
-#define RELOC_RELATIVE  8   /* Adjust by program base */
-#define RELOC_GOTOFF    9   /* 32 bit offset to GOT */
-#define RELOC_GOTPC     10   /* 32 bit PC relative offset to GOT */
-#define RELOC_16        20 
-#define RELOC_PC16      21 
-#define RELOC_8         22 
-#define RELOC_PC8       23 
+#define	R_TYPE(X)       __CONCAT(RELOC_,X)
 
-#endif /* _MACH_RELOC_H */
+#define	RELOC_NONE	0	/* No reloc */
+#define	RELOC_32	1	/* Direct 32 bit  */
+#define	RELOC_PC32	2	/* PC relative 32 bit */
+#define	RELOC_GOT32	3	/* 32 bit GOT entry */
+#define	RELOC_PLT32	4	/* 32 bit PLT address */
+#define	RELOC_COPY	5	/* Copy symbol at runtime */
+#define	RELOC_GLOB_DAT	6	/* Create GOT entry */
+#define	RELOC_JUMP_SLOT	7	/* Create PLT entry */
+#define	RELOC_RELATIVE	8	/* Adjust by program base */
+#define	RELOC_GOTOFF	9	/* 32 bit offset to GOT */
+#define	RELOC_GOTPC	10	/* 32 bit PC relative offset to GOT */
+#define	RELOC_16	20
+#define	RELOC_PC16	21
+#define	RELOC_8		22
+#define	RELOC_PC8	23
 
-
+#endif /* _I386_RELOC_H_ */
