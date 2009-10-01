137bae6b3bc87eec560358f2cfaee6b99663f51c
pedro <pedro>
2009-10-01 20:16:41 +0000

    sorting is pointless, from netbsd, pointed out and okay'd by mickey@

--- bin/cp/cp.c
+++ bin/cp/cp.c
@@ -71,7 +71,7 @@ static const char copyright[] =
 #if 0
 static char sccsid[] = "@(#)cp.c        8.5 (Berkeley) 4/29/95";
 #else
-static const char rcsid[] = "$ABSD$";
+static const char rcsid[] = "$ABSD: cp.c,v 1.1.1.1 2008/08/26 14:36:26 root Exp $";
 #endif
 #endif /* not lint */
 
@@ -86,7 +86,6 @@ mode_t myumask;
 enum op { FILE_TO_FILE, FILE_TO_DIR, DIR_TO_DNE };
 
 int copy(char *[], enum op, int);
-int mastercmp(const FTSENT **, const FTSENT **);
 char *find_last_component(char *);
 
 int
@@ -269,7 +268,7 @@ copy(char *argv[], enum op type, int fts_options)
 	char *p, *target_mid;
 	base = 0;
 
-	if ((ftsp = fts_open(argv, fts_options, mastercmp)) == NULL)
+	if ((ftsp = fts_open(argv, fts_options, NULL)) == NULL)
 		err(1, NULL);
 	for (rval = 0; (curr = fts_read(ftsp)) != NULL;) {
 		switch (curr->fts_info) {
@@ -454,30 +453,4 @@ copy(char *argv[], enum op type, int fts_options)
 		err(1, "fts_read");
 	(void)fts_close(ftsp);
 	return (rval);
-}
-
-/*
- * mastercmp --
- *	The comparison function for the copy order.  The order is to copy
- *	non-directory files before directory files.  The reason for this
- *	is because files tend to be in the same cylinder group as their
- *	parent directory, whereas directories tend not to be.  Copying the
- *	files first reduces seeking.
- */
-int
-mastercmp(const FTSENT **a, const FTSENT **b)
-{
-	int a_info, b_info;
-
-	a_info = (*a)->fts_info;
-	if (a_info == FTS_ERR || a_info == FTS_NS || a_info == FTS_DNR)
-		return (0);
-	b_info = (*b)->fts_info;
-	if (b_info == FTS_ERR || b_info == FTS_NS || b_info == FTS_DNR)
-		return (0);
-	if (a_info == FTS_D)
-		return (-1);
-	if (b_info == FTS_D)
-		return (1);
-	return (0);
 }
