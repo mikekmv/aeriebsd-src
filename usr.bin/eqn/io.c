87cebc59508eab5db4f8839591d5cb8e9107fa18
mickey <mickey>
2009-10-13 22:25:23 +0000

    put global vars together w/ main(); mwa@ ok

--- usr.bin/eqn/io.c
+++ usr.bin/eqn/io.c
@@ -68,18 +68,48 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
+#if 0
+static char sccsid[] = "@(#)glob.c	4.3 (Berkeley) 4/17/91";
 static char sccsid[] = "@(#)io.c	4.6 (Berkeley) 4/17/91";
+#else
+static const char rcsid[] = "$ABSD$";
+#endif
 #endif /* not lint */
 
 # include "e.h"
 #define	MAXLINE	1200	/* maximum input line */
 
 char	in[MAXLINE];	/* input buffer */
-int	eqnexit();
-int noeqn;
+int	noeqn;
 
-main(argc,argv) int argc; char *argv[];{
+int	dbg;	/* debugging print if non-zero */
+int	lp[80];	/* stack for things like piles and matrices */
+int	ct;	/* pointer to lp */
+int	used[100];	/* available registers */
+int	ps;	/* default init point size */
+int	deltaps	= 3;	/* default change in ps */
+int	gsize	= 10;	/* default initial point size */
+int	gfont	= ITAL;	/* italic */
+int	ft;	/* default font */
+FILE	*curfile;	/* current input file */
+int	ifile;
+int	linect;	/* line number in file */
+int	eqline;	/* line where eqn started */
+int	svargc;
+char	**svargv;
+int	eht[100];
+int	ebase[100];
+int	lfont[100];
+int	rfont[100];
+int	eqnreg;	/* register where final string appears */
+int	eqnht;	/* inal height of equation */
+int	lefteq	= '\0';	/* left in-line delimiter */
+int	righteq	= '\0';	/* right in-line delimiter */
+int	lastchar;	/* last character read by lex */
+int	markline	= 0;	/* 1 if this EQ/EN contains mark or lineup */
 
+main(int argc, char *argv[])
+{
 	eqnexit(eqn(argc, argv));
 }
 
