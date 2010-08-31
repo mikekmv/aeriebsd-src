dc8ec7cbe7b910cde29a95c120b281058863f28b
mickey <mickey>
2010-08-31 16:37:22 +0000

    from mpf@: Fix a 16 year old bug in the sorting routine for non-contiguous netmasks. For masks of identical length rn_lexobetter() did not stop on the first non-equal byte. This leads rn_addroute() to not detecting duplicate entries and thus we might create a very long list of masks to check for each node. This can have a huge impact on IPsec performance, where non-contiguous masks are used for the flow lookup.  In a setup with 1300 flows we saw 400 duplicate masks and only a third of the expected throughput. Lots of help in narrowing this down from markus@. Improved comments from claudio@.

--- sys/net/radix.c
+++ sys/net/radix.c
@@ -470,13 +470,18 @@ rn_lexobetter(void *m_arg, void *n_arg)
 {
 	u_char *mp = m_arg, *np = n_arg, *lim;
 
+	/*
+	 * longer masks might not really be lexicographically better,
+	 * but since trailing zeros were stipped from the mask,
+	 * we need to check the longer one first.
+	 */
 	if (*mp > *np)
-		return 1;  /* not really, but need to check longer one first */
-	if (*mp == *np)
-		for (lim = mp + *mp; mp < lim;)
-			if (*mp++ > *np++)
-				return 1;
-	return 0;
+		return 1;
+	if (*mp < *np)
+		return 0;
+
+	lim = mp + *mp;
+	return (memcmp(mp, np, *lim) > 0);
 }
 
 static struct radix_mask *
