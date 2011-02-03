06937bacc4c1e78df9b45124506b6e78a034fa01
mickey <mickey>
2011-02-03 23:19:54 +0000

    fix the notes

--- lib/csu/common_elf/os-note-elf.h
+++ lib/csu/common_elf/os-note-elf.h
@@ -4,15 +4,16 @@
  *  long Name length
  *  long Description length
  *  long ELF_NOTE_TYPE_OSVERSION (1) XXX - need a define.
- *  "OpenBSD\0"
+ *  "AerieBSD\0"
  *  version? 0 XXX
  */
 
-__asm("	.section \".note.openbsd.ident\", \"a\"\n"
+__asm("	.section \".note.aeriebsd.ident\", \"a\"\n"
 "	.p2align 2\n"
-"	.long	8\n"
+"	.long	16\n"
 "	.long	4\n"
 "	.long	1\n"
-"	.ascii \"OpenBSD\\0\"\n"
+"	.ascii \"AerieBSD\\0\"\n"
+"	.p2align 4\n"
 "	.long	0\n"
 "	.p2align 2");
