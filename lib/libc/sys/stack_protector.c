d4ec6cc3656750b572554f4a6d48762089d2ef26
mickey <mickey>
2011-07-20 14:14:58 +0000

    do not pull in half a libc with just a stack protector; pedro ok

--- lib/libc/sys/stack_protector.c
+++ lib/libc/sys/stack_protector.c
@@ -1,5 +1,5 @@
-
 /*
+ * Copyright (c) 2011 Michael Shalayeff
  * Copyright (c) 2002 Hiroaki Etoh, Federico G. Schwindt, and Miodrag Vallat.
  * All rights reserved.
  *
@@ -27,62 +27,106 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char rcsid[] = "$ABSD: stack_protector.c,v 1.1.1.1 2008/08/26 14:38:39 root Exp $";
+static const char rcsid[] = "$ABSD: stack_protector.c,v 1.2 2010/05/14 14:08:10 mickey Exp $";
 #endif
 
 #include <sys/param.h>
+#include <sys/socket.h>
 #include <sys/sysctl.h>
+#include <sys/fcntl.h>
+#include <sys/uio.h>
+#include <sys/un.h>
 #include <signal.h>
 #include <string.h>
 #include <syslog.h>
+#include <paths.h>
 #include <unistd.h>
 
 extern int __sysctl(int *, u_int, void *, size_t *, void *, size_t);
 
-long __guard[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+long __stack_chk_guard[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+__weak_alias(__guard, __stack_chk_guard);
 static void __guard_setup(void) __attribute__ ((constructor));
-void __stack_smash_handler(char func[], int damaged __attribute__((unused)));
+void __stack_chk_fail(char func[]);
+__weak_alias(__stack_smash_handler, __stack_chk_fail);
 
-#pragma constructor
-static void
+static void __attribute__ ((constructor))
 __guard_setup(void)
 {
 	int mib[2];
 	size_t len;
 
-	if (__guard[0] != 0)
+	if (__stack_chk_guard[0] != 0)
 		return;
 
 	mib[0] = CTL_KERN;
 	mib[1] = KERN_ARND;
 
-	len = sizeof(__guard);
-	if (__sysctl(mib, 2, __guard, &len, NULL, 0) == -1 ||
-	    len != sizeof(__guard)) {
+	len = sizeof(__stack_chk_guard);
+	if (__sysctl(mib, 2, __stack_chk_guard, &len, NULL, 0) == -1 ||
+	    len != sizeof(__stack_chk_guard)) {
 		/* If sysctl was unsuccessful, use the "terminator canary". */
-		((unsigned char *)__guard)[0] = 0;
-		((unsigned char *)__guard)[1] = 0;
-		((unsigned char *)__guard)[2] = '\n';
-		((unsigned char *)__guard)[3] = 255;
+		((unsigned char *)__stack_chk_guard)[0] = 0;
+		((unsigned char *)__stack_chk_guard)[1] = 0;
+		((unsigned char *)__stack_chk_guard)[2] = '\n';
+		((unsigned char *)__stack_chk_guard)[3] = 255;
 	}
 }
 
 /*ARGSUSED*/
 void
-__stack_smash_handler(char func[], int damaged)
+__stack_chk_fail(char func[])
 {
-	struct syslog_data sdata = SYSLOG_DATA_INIT;
-	const char message[] = "stack overflow in function %s";
+	extern char *__progname;
+	static char pri[] = "<2>";	/* LOG_CRIT */
+	static char message[] = ": stack overflow in function ";
+	struct sockaddr_un SyslogAddr;	/* AF_UNIX address of local logger */
+	struct iovec iov[5], *iop = iov;
 	struct sigaction sa;
 	sigset_t mask;
+	int fd, n = 5;
 
 	/* Immediately block all signal handlers from running code */
 	sigfillset(&mask);
 	sigdelset(&mask, SIGABRT);
 	sigprocmask(SIG_BLOCK, &mask, NULL);
 
-	/* This may fail on a chroot jail... */
-	syslog_r(LOG_CRIT, &sdata, message, func);
+	memset(&SyslogAddr, '\0', sizeof SyslogAddr);
+	SyslogAddr.sun_len = sizeof SyslogAddr;
+	SyslogAddr.sun_family = AF_UNIX;
+	strlcpy(SyslogAddr.sun_path, _PATH_LOG, sizeof SyslogAddr.sun_path);
+	if ((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) >= 0) {
+		if (connect(fd, (struct sockaddr *)&SyslogAddr,
+		    sizeof SyslogAddr)) {
+			close(fd);
+			fd = -1;
+			n = 4;
+			iop[3].iov_base = "\r\n";
+			iop[3].iov_len = 2;
+		} else {
+			iop[0].iov_base = pri;
+			iop[0].iov_len = sizeof pri - 1;
+			iop++;
+			iop[3].iov_base = "\n";
+			iop[3].iov_len = 1;
+		}
+	}
+
+	/*
+         * Output the message to the console; try not to block
+	 * as a blocking console should not stop other processes.
+	 */
+	if (fd < 0 && (fd = open(_PATH_CONSOLE, O_WRONLY|O_NONBLOCK, 0)) < 0)
+		fd = STDERR_FILENO;
+
+	iop[0].iov_base = __progname;
+	iop[0].iov_len = strlen(__progname);
+	iop[1].iov_base = message;
+	iop[1].iov_len = sizeof message - 1;
+	iop[2].iov_base = func;
+	iop[2].iov_len = strlen(func);
+	(void)writev(fd, iov, n);
+	(void)close(fd);
 
 	bzero(&sa, sizeof(struct sigaction));
 	sigemptyset(&sa.sa_mask);
